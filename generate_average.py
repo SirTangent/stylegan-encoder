897c6fcbee8e84acb9f9fc6135c6f20c8180f5a9
Wyatt Phillips <phillipsw1@wit.edu>
2020-10-23 13:19:28 -0400

    Add todo item for gen

--- generate_average.py
+++ generate_average.py
@@ -17,12 +17,13 @@ tflib.init_tf()
 with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
     generator_network, discriminator_network, Gs_network = pickle.load(f)
 
+# TODO: Implement Dynamic Loading
 # load the latents
-s1 = np.load('images-latent/Sub-directory-name/image1-name_01.npy')
-s2 = np.load('images-latent/Sub-directory-name/image2-name_01.npy')
-s3 = np.load('images-latent/Sub-directory-name/image3-name_01.npy')
-s4 = np.load('images-latent/Sub-directory-name/image4-name_01.npy')
-s5 = np.load('images-latent/Sub-directory-name/image5-name_01.npy')
+s1 = np.load('images-latent/test_1/a1_01.npy')
+s2 = np.load('images-latent/test_1/a2_01.npy')
+s3 = np.load('images-latent/test_1/a3_01.npy')
+s4 = np.load('images-latent/test_1/a4_01.npy')
+s5 = np.load('images-latent/test_1/a5_01.npy')
 
 s1 = np.expand_dims(s1,axis=0)
 s2 = np.expand_dims(s2,axis=0)
@@ -38,8 +39,8 @@ savg = (1/5)*(s1+s2+s3+s4+s5)
 # run the generator network to render the latents:
 synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=False), minibatch_size=8)
 images = Gs_network.components.synthesis.run(savg, randomize_noise=False, **synthesis_kwargs)
-display(PIL.Image.fromarray(images.transpose((0,2,3,1))[0], 'RGB').resize((512,512),PIL.Image.LANCZOS))
+#display(PIL.Image.fromarray(images.transpose((0,2,3,1))[0], 'RGB').resize((512,512),PIL.Image.LANCZOS))
 
-out_name = save_dir + "Sub-directory-name" + ".png"
+out_name = save_dir + "test_1" + ".png"
 img = PIL.Image.fromarray(images.transpose((0,2,3,1))[0], 'RGB').resize((512,512),PIL.Image.LANCZOS)
 img.save(out_name)
