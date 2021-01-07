63886406c7175bdd2a0304bef6606dcf45c85f6b
Wyatt Phillips <phillipsw1@wit.edu>
2021-01-07 13:03:15 -0500

    Include video output parameter

--- runtime.py
+++ runtime.py
@@ -1,5 +1,6 @@
 import os
 
+
 class runtime:
     def __init__(self, img_dir="images/"):
         self.dir = img_dir
@@ -13,7 +14,7 @@ class runtime:
         # Run asynchronously
         return os.system(f"python align_images.py {src_dir} {target_dir}")
 
-    def run_encoder(self, aligned_dir="", gen_dir="", latent_dir=""):
+    def run_encoder(self, aligned_dir="", gen_dir="", latent_dir="", output_video=True):
         if aligned_dir == "":
             aligned_dir = os.path.join(self.dir, "images-aligned")
         if gen_dir == "":
@@ -22,7 +23,8 @@ class runtime:
             latent_dir = os.path.join(self.dir, "images-latent")
 
         # Run asynchronously
-        return os.system(f"python encode_images.py --batch_size=1 --output_video=False {aligned_dir} {gen_dir} {latent_dir}")
+        return os.system(
+            f"python encode_images.py --batch_size=1 --output_video={'True' if output_video else 'False'} {aligned_dir} {gen_dir} {latent_dir}")
 
     def run_averager(self, latent_dir="", output_file=""):
         if latent_dir == "":
